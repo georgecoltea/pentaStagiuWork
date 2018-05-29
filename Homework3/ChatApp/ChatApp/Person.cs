ef823790cf577ed15e0af6cc3d74b19a6e12698e
George Coltea <george.coltea16@gmail.com>
2018-05-29 21:33:50 +0300

    add homework 3 classes

---
+++ Homework3/ChatApp/ChatApp/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChatApp
+{
+    public class User
+    {
+		private int id;
+		private string firstName;
+		private string lastName;
+		private string email;
+		private string password;
+
+		public int Id { get; set; }
+        
+		public string FirstName { get;  set; }
+
+		public string LastName { get;  set; }
+        
+		public string Email { get;  set; }
+
+		public string Password { get; set; }
+
+		public User(){}
+
+		public User(int id, string firstName, string lastName, string email, string password)
+		{
+			this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+			this.Email = email;
+			this.Password = password;
+		}
+	}
+
+	public class Post
+    {
+		private int id;
+		private User author;
+		private string message;
+		private DateTime date;
+
+		public int Id { get; set; }
+		public User Author { get; set; }
+		public string Message { get; set; }
+		public DateTime Date { get; set; }
+
+		public Post(){}
+
+		public Post(int id, User author, string message, DateTime date)
+        {
+            this.Id = id;
+			this.Author = author;
+			this.Message = message;
+			this.Date = date;
+            
+        }
+    }
+
+	public class Board
+    {
+		private List<Post> posts;
+
+		public List<Post> Posts { get; set; }
+
+		public Board(){}
+    }
+    
+}
---
+++ Homework3/ChatApp/ChatApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChatApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			User george = new User
+			{
+				Id = 1,
+				FirstName = "George",
+				LastName = "Coltea",
+                Email = "george@gmail.com",
+                Password = "parola"
+
+            };
+
+
+			Post post1 = new Post
+			{
+				Id = 1,
+				Author = george,
+				Message = "Am scris trei noi clase",
+				Date = new DateTime(2018, 5, 28)
+
+			};
+
+			Board mainBoard = new Board();
+			mainBoard.Posts = new List<Post>();
+			mainBoard.Posts.Add(post1);
+
+			Console.WriteLine(post1);
+				
+
+        }
+    }
+}
