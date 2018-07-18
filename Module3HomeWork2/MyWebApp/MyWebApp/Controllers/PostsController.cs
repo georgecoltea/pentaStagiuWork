83921bd7da68a58dae079402180d651991ee0506
George Coltea <george.coltea16@gmail.com>
2018-07-18 14:11:27 +0300

    Homework 2 done, including missing functionality from Homework 1

---
+++ Module3HomeWork2/MyWebApp/MyWebApp/Controllers/PostsController.cs
@@ -0,0 +1,97 @@
+﻿using MyWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebApp.Controllers
+{
+    public class PostsController : Controller
+    {
+        public static List<Post> posts = new List<Post>();
+        public static int PostId = 0;
+        // GET: Posts
+
+        public ActionResult Index()
+        {
+            ViewBag.CurrentDateTime = DateTime.Now;
+            return View(posts);
+        }
+
+        [HttpGet]
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Create(Post post)
+        {
+            if(post == null)
+            {
+                HttpNotFound();
+            }
+
+            PostId++;
+            post.Id = PostId;
+            posts.Add(post);
+
+            return RedirectToAction("Details");
+
+        }
+
+        [HttpGet]
+
+        public ActionResult Details()
+        {
+            int lastPost = posts.Count;
+            Post post = posts.Find(p => p.Id == lastPost);
+            return View(post);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Post post = posts.Find(p => p.Id == id);
+
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+            posts.Remove(post);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Post post = posts.Find(p => p.Id == id);
+
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(post);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Post post)
+        {
+            Post postFromList = posts.Find(p => p.Id == post.Id);
+            postFromList.UserId = post.UserId;
+            postFromList.IsSticky = post.IsSticky;
+            postFromList.Message = post.Message;
+            postFromList.Priority = post.Priority;
+            postFromList.TimeOfPosting = post.TimeOfPosting;
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Module3HomeWork2/MyWebApp/MyWebApp/Models/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWebApp.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime TimeOfPosting { get; set; }
+        public string Message { get; set; }
+        public MessageType PostType { get; set; }
+        public bool IsSticky { get; set; }
+        public int Priority { get; set; }
+    }
+
+    public enum MessageType
+    {
+        Text,
+        Photo
+    }
+}
