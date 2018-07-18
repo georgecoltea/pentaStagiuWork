c0f61415c7870c0c5223e4410f75c375f20c68d6
George Coltea <george.coltea16@gmail.com>
2018-07-18 11:24:56 +0300

    Add post message functionality, to add delete and edit

---
+++ Module3HomeWork1/MyWebApp/MyWebApp/Controllers/PostsController.cs
@@ -0,0 +1,55 @@
+﻿using MyWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebApp.Controllers
+{
+    public class PostsController : Controller
+    {
+        public static List<Post> posts = new List<Post>();
+        public static int PostId = 0;
+        // GET: Posts
+
+        public ActionResult Index()
+        {
+            return View(posts);
+        }
+
+        [HttpGet]
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Create(Post post)
+        {
+            if(post == null)
+            {
+                HttpNotFound();
+            }
+
+            PostId++;
+            post.Id = PostId;
+            posts.Add(post);
+
+            return RedirectToAction("Details");
+
+        }
+
+        [HttpGet]
+
+        public ActionResult Details()
+        {
+            int lastPost = posts.Count;
+            Post post = posts.Find(p => p.Id == lastPost);
+            return View(post);
+        }
+
+    }
+}
