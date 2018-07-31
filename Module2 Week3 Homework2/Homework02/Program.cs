80afb58e67bffabaa730997ccc2f02f169ca8295
George Coltea <george.coltea16@gmail.com>
2018-07-31 19:10:49 +0300

    add username

---
+++ Module2 Week3 Homework2/Homework02/Program.cs
@@ -0,0 +1,36 @@
+﻿using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static Repository.JsonHelper;
+
+namespace Homework02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = ExtractJson();
+
+            var result = persons
+                .Select( p => new User()
+                {
+                    Username = $"{p.FirstName}{p.LastName}{p.BirthDate.ToString("yy")}",
+                    FirstName = p.FirstName,
+                    LastName = p.LastName,
+                    Gender = p.Gender,
+                    BirthDate = p.BirthDate
+                })
+                .ToList();
+
+            foreach (User user in result)
+            {
+                Console.WriteLine(user);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module2 Week3 Homework2/Repository/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repository
+{
+    public class User : Person
+    {
+        private Guid _id;
+        public string Username { get; set; }
+
+        public User(string username, string firstName, string lastName, Gender gender, DateTime birthDate) : base(firstName, lastName, gender, birthDate)
+        {
+            this._id = Guid.NewGuid();
+        }
+
+        public User()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{Username} {FirstName} {LastName} {Gender} {BirthDate}";
+        }
+    }
+}
+
